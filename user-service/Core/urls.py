e68a1779cd03433e26624e8dec67a5c4a026cb6c
salman4mtest <salmanmemmedli01@gmail.com>
2025-11-09 22:56:10 +0400

    still can't handle user connection by traefik

--- user-service/Core/urls.py
+++ user-service/Core/urls.py
@@ -1,20 +1,14 @@
 from django.contrib import admin
-from django.contrib import admin
 from django.urls import path, include
-
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework.renderers import JSONRenderer
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-   path('user-admin/', admin.site.urls),
-
-    path('', include('user_service.urls')),
-    
+    path('user-admin/', admin.site.urls),
+    path('', include('user_service.urls')),  # Remove 'api/' prefix here
 ]
 
 urlpatterns += (
-     # Swagger & Redoc documentation
     path('openapi.json', SpectacularAPIView.as_view(api_version='1.0', renderer_classes=[JSONRenderer]), name='schema'),
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
--- user-service/user_service/urls.py
+++ user-service/user_service/urls.py
@@ -7,13 +7,12 @@ from .views import (
     PasswordResetConfirmView,
     validate_credentials,
 )
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    # path('login/', LoginView.as_view(), name='login'),
-    path('internal/validate-credentials/', validate_credentials),  
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', UserProfileView.as_view(), name='user-profile'),
-    path('password-reset/request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
-    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
+urlpatterns = [
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/internal/validate-credentials/', validate_credentials),  
+    path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('api/password-reset/request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('api/password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
